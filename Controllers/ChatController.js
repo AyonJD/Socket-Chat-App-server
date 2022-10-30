5234ee1717cf7cc84ab9991dfccac680f60ceead
Ayon Jodder <ayonjodder177@gmail.com>
2022-10-30 23:19:47 +0600

    Chat controller

--- Controllers/ChatController.js
+++ Controllers/ChatController.js
@@ -4,4 +4,11 @@ module.exports.createChat = async (req, res) => {
     const newChat = new ChatModel({
         members: [req.body.senderId, req.body.receiverId]
     })
+
+    try {
+        const result = await newChat.save()
+        res.status(200).json({ success: true, message: "Chat created successfully", result })
+    } catch (err) {
+        res.status(500).json({ success: false, message: "Internal server error", err })
+    }
 }
