86d2650a39f0ddfe0e36b8596a29962b4ec388de
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-06-10 23:02:18 +0600

    [Done] Login solved

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -9,7 +9,7 @@ module.exports.createUser = async (req, res) => {
     });
 
     try {
-        const accessToken = jwt.sign(req.body, process.env.TOKEN, { expiresIn: "1h" });
+        const accessToken = jwt.sign(req.body.email, process.env.TOKEN, { expiresIn: "1h" });
         const result = await newUser.save();
         const { username, email } = result;
         res.send({ success: true, message: "Successfully created user", token: accessToken, result: { username, email } });
@@ -40,27 +40,48 @@ module.exports.loginUser = async (req, res) => {
     try {
         const { username_or_email, password } = req.body;
         const user = await UserModel.findOne({ $or: [{ username: username_or_email }, { email: username_or_email }] });
-
         if (!user) {
-            throw new Error(400, 'User not found')
+            throw new Error(400, 'User not found');
         }
-        const id = user._id.toString().split('"')[0];
 
-        const storedPassword = user?.password
-        const passwordMatch = storedPassword === password
+        const storedPassword = user?.password;
+        const passwordMatch = storedPassword === password;
 
         if (!passwordMatch) {
-            throw new Error(400, 'Invalid password')
+            throw new Error(400, 'Invalid password');
         }
 
         if (user) {
             const { username, email } = user;
-            const accessToken = jwt.sign(req.body, process.env.TOKEN, { expiresIn: "1h" });
-            res.status(200).json({ success: true, message: "Login successfully", token: accessToken, result: { id, username, email } });
+            const accessToken = jwt.sign({ email }, process.env.TOKEN, { expiresIn: "1d" });
+
+            res.status(200).json({ success: true, message: "Login successfully", token: accessToken, result: { username, email } });
         } else {
             res.status(404).json({ success: false, message: "User not found" });
         }
     } catch (err) {
         res.status(500).json({ success: false, message: "Internal server error", error: err });
     }
-}
+};
+
+module.exports.loggedInUser = async (req, res) => {
+    try {
+        const authorizationHeader = await req.headers.authorization;
+
+        if (!authorizationHeader) {
+            res.status(401).json({ success: false, message: "Authorization header missing" });
+            return;
+        }
+
+        const token = authorizationHeader.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.TOKEN);
+        const user = await UserModel.findOne({ email: decoded.email }).select({ password: 0, _v: 0 });
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        res.status(200).json({ success: true, message: "User fetched successfully", result: user });
+    } catch (err) {
+        res.status(500).json({ success: false, message: "Internal server error", error: err });
+    }
+};
--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createUser, getUser, loginUser, excludeCurrentUser } = require('../Controllers/UserController');
+const { createUser, getUser, loginUser, excludeCurrentUser, loggedInUser } = require('../Controllers/UserController');
 const router = express.Router();
 
 router.get("/", getUser);
 router.post('/signup', createUser);
 router.post('/login', loginUser);
 router.get('/exclude/:userId', excludeCurrentUser);
+router.post('/loggedInUser', loggedInUser)
 
 module.exports = router;
